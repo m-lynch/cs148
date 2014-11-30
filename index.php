3408018fae2c8175985fa81300baa6bb5eb8f94b
m-lynch <mklynch@uvm.edu>
2014-11-30 12:12:53 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<?php
+include "top.php"; 
+
+?>
+<body id="home">
+
+
+<article id="main">
+	<h1>Welcome</h1>
+        
+        <p>He at Mike Lynch's Legendary Libary we have a variety of books and movies that our members can sign out.  Go to the Book Search page or the Movie Search page to see exactly what we have to offer.  If you are not a member you can go to the member join page where you can enter your personal information and become a member.  This allows you to reserve movies or books when you are unable to pick them up that day.  You can make these reservations on the Item Reserve page.</p>
+        
+        <h2>Facts About Mike</h2>
+        <ul>
+            <li>Favorite Food: Pizza Obviously</li>
+            <li>Favorite Type of Music: Rap</li>
+            <li>Favorite Animal: Tiger</li>
+            <li>Favorite Sports Team: New England Patriots</li>
+        </ul>
+        <p>Below is a picture of me and my father skiing at Stowe Mountain Resort</p>
+        <img src="pops.JPG" alt="original"/>
+</article>
+
+<aside id="other">
+    <h2>Mike's Hobbies</h2>
+    <ul>
+        <li>Skiing</li>
+        <li>Hiking</li>
+        <li>Lacrosse</li>
+        <li>Rugby</li>
+        <li>Anything Exciting (See below)</li>
+    </ul>
+    
+    <img src="skydive.JPG" alt="Skydiving">
+    <p>Above is a picture of me moments before I jumped out of a plane for my 18th birthday.</p>
+    </aside>
+    
+
+
+<? include ("footer.php"); ?>
+
+</body>
+</html>
