01ca1567663d4df6dc30000f23bedb77f5b98ef6
m-lynch <mklynch@uvm.edu>
2014-12-01 14:37:04 -0500

    Create nav.php

---
+++ nav.php
@@ -0,0 +1,9 @@
+<nav>
+     <ol>
+        <li><a href="index.php">home</a></li>
+        <li><a href="bookSearch.php">Search for books</a></li>
+     	<li><a href="movieSearch.php">Search for Movies</a></li>
+        <li><a href="memberJoin.php">Join our Library</a></li>
+        <li><a href="ItemReserve.php">Reserve an Item</a></li>
+     </ol>
+</nav>
